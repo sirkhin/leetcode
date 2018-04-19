f4589bd2e2002faccc7b590853e665dafbd97597
viaches <i.am.viaches@gmail.com>
2018-04-19 23:24:07 +0300

    Added solution for 461. Hamming Distance (https://leetcode.com/problems/hamming-distance/description/)

---
+++ bitManipulation/461_HammingDistance/HammingDistance/HammingDistance/HammingDistanceCounter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HammingDistance
+{
+    /*
+        461. Hamming Distance
+
+        The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+        Given two integers x and y, calculate the Hamming distance.
+
+        Note:
+        0 ≤ x, y < 231.
+
+        Example:
+
+        Input: x = 1, y = 4
+
+        Output: 2
+
+        Explanation:
+        1   (0 0 0 1)
+        4   (0 1 0 0)
+               ↑   ↑
+
+        The above arrows point to positions where the corresponding bits are different.
+     */
+    public class HammingDistanceCounter
+    {
+        public int CountHammingDistance(int x, int y)
+        {
+            var byteX = GetBinaryRepresentation(x);
+            var byteY = GetBinaryRepresentation(y);
+
+            var max = Math.Max(byteX.Length, byteY.Length);
+
+            if (byteX.Length < max)
+            {
+                var list = byteX.ToList();
+                for (int j = 0; j < max - byteX.Length; j++)
+                {
+                    list.Insert(j, false);
+                }
+
+                byteX = list.ToArray();
+            }
+            else if (byteY.Length < max)
+            {
+                var list = byteY.ToList();
+                for (int j = 0; j < max - byteY.Length; j++)
+                {
+                    list.Insert(j, false);
+                }
+
+                byteY = list.ToArray();
+            }
+
+            var result = new bool[max];
+
+            for (int i = 0; i < max; i++)
+            {
+                result[i] = (byteX[i] ^ byteY[i]);
+            }
+
+            var count = result.Count(r => r);
+
+            return count;
+        }
+
+        private static bool[] GetBinaryRepresentation(int i)
+        {
+            List<bool> result = new List<bool>();
+            while (i > 0)
+            {
+                int m = i % 2;
+                i = i / 2;
+                result.Add(m == 1);
+            }
+            result.Reverse();
+            return result.ToArray();
+        }
+    }
+}
---
+++ bitManipulation/461_HammingDistance/HammingDistance/HammingDistance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammingDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 4;
+            var y = 14;
+
+            var solver = new HammingDistanceCounter();
+
+            var count = solver.CountHammingDistance(x, y);
+
+            Console.WriteLine($"Hamming Distance is {count}");
+            Console.ReadLine();
+        }
+    }
+}
