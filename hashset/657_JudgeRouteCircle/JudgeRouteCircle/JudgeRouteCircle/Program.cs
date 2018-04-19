d9efc39dc6772a3be35e64e49a9243422930922d
viaches <i.am.viaches@gmail.com>
2018-04-19 23:59:42 +0300

    Added solution for 657. Judge Route Circle (https://leetcode.com/problems/judge-route-circle/description/)

---
+++ hashset/657_JudgeRouteCircle/JudgeRouteCircle/JudgeRouteCircle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JudgeRouteCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var moves = "LLRRUD";
+
+            var solver = new RouteCircleFinder();
+
+            var isCircle = solver.CheckIfCircleRoute(moves);
+
+            Console.WriteLine($"Is circle? {isCircle}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hashset/657_JudgeRouteCircle/JudgeRouteCircle/JudgeRouteCircle/RouteCircleFinder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace JudgeRouteCircle
+{
+    /*
+        657. Judge Route Circle
+
+        Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
+
+        The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). The output should be true or false representing whether the robot makes a circle.
+
+        Example 1:
+        Input: "UD"
+        Output: true
+        Example 2:
+        Input: "LL"
+        Output: false
+     */
+    public class RouteCircleFinder
+    {
+        public bool CheckIfCircleRoute(string moves)
+        {
+            var movesDict = new Dictionary<char, int>();
+
+            movesDict.Add('L', 1);
+            movesDict.Add('R', 1);
+            movesDict.Add('U', 1);
+            movesDict.Add('D', 1);
+
+            foreach (char t in moves)
+            {
+                if (movesDict.ContainsKey(t))
+                {
+                    movesDict[t]++;
+                }
+                else
+                {
+                    movesDict.Add(t, 1);
+                }
+            }
+
+            return movesDict['L'] == movesDict['R'] && movesDict['U'] == movesDict['D'];
+        }
+    }
+}
