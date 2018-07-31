33a3d336767aad391d4db3fa095e6600177dfb5c
viaches <i.am.viaches@gmail.com>
2018-08-01 00:06:29 +0300

    Added solution for 547: Friend Circles (https://leetcode.com/problems/friend-circles/description/)

---
+++ unionFind/547_FindFriendCircles/FindFriendCircles/FindFriendCircles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindFriendCircles
+{
+    class Program
+    {
+        public int FindCircleNum(int[,] M)
+        {
+            var numberOfStudents = M.GetLength(0);
+
+            var unionFind = new WeightedUnionFind(numberOfStudents);
+
+            for (int i = 0; i < numberOfStudents; i++)
+            for (int j = i + 1; j < numberOfStudents; j++)
+            {
+                unionFind.Union(i, j, M[i, j]);
+            }
+
+            return unionFind.FriendCirclesCount;
+        }
+
+        static void Main(string[] args)
+        {
+            var M = new int[3, 3] {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+
+            var program = new Program();
+
+            var count = program.FindCircleNum(M);
+
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ unionFind/547_FindFriendCircles/FindFriendCircles/FindFriendCircles/WeightedUnionFind.cs
@@ -0,0 +1,60 @@
+﻿namespace FindFriendCircles
+{
+    public class WeightedUnionFind
+    {
+        private int[] _id;
+        private int[] _size;
+
+        public int FriendCirclesCount;
+
+        public WeightedUnionFind(int n)
+        {
+            _id = new int[n];
+            _size = new int[n];
+            FriendCirclesCount = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                _id[i] = i;
+                _size[i] = 1;
+            }
+        }
+
+        public void Union(int p, int q, int shouldBeConnected)
+        {
+            if (!Connected(p, q) && shouldBeConnected == 1)
+            {
+                int pid = Root(p);
+                int qid = Root(q);
+
+                if (_size[p] >= _size[q])
+                {
+                    _id[qid] = pid;
+                    _size[p] += _size[q];
+                }
+                else
+                {
+                    _id[pid] = qid;
+                    _size[q] += _size[p];
+                }
+
+                FriendCirclesCount--;
+            }
+        }
+
+        private bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        private int Root(int i)
+        {
+            while (_id[i] != i)
+            {
+                i = _id[i];
+            }
+
+            return i;
+        }
+    }
+}
