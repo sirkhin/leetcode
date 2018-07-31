29eaa9e208b430ea433750ac3d41b90571243227
viaches <i.am.viaches@gmail.com>
2018-08-01 00:53:47 +0300

    Added solution for 684. Redundant Connection (https://leetcode.com/problems/redundant-connection/description/)

---
+++ unionFind/684_RedundantConnection/RedundantConnection/RedundantConnection/WeightedUnionFind.cs
@@ -0,0 +1,55 @@
+﻿namespace RedundantConnection
+{
+    class WeightedUnionFind
+    {
+        private int[] _id;
+        private int[] _size;
+
+        public WeightedUnionFind(int n)
+        {
+            _id = new int[n + 1];
+            _size = new int[n + 1];
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                _id[i] = i;
+                _size[i] = 1;
+            }
+        }
+
+        public void Union(int p, int q)
+        {
+            if (!Connected(p, q))
+            {
+                int pid = Root(p);
+                int qid = Root(q);
+
+                if (_size[p] >= _size[q])
+                {
+                    _id[qid] = pid;
+                    _size[p] += _size[q];
+                }
+                else
+                {
+                    _id[pid] = qid;
+                    _size[q] += _size[p];
+                }
+            }
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        private int Root(int i)
+        {
+            while (_id[i] != i)
+            {
+                i = _id[i];
+            }
+
+            return i;
+        }
+    }
+}
