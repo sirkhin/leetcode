60c44392d060667f6cadc2fbca3913e7923bfca8
viaches <i.am.viaches@gmail.com>
2018-08-05 22:23:12 +0300

    Added solution for 155: Min Stack (https://leetcode.com/problems/min-stack/)

---
+++ stack/155_MinStack/MinStack/MinStack/MinStack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace MinStack
+{
+    internal class MinStack
+    {
+        private class Node
+        {
+            public int Item { get; set; }
+
+            public int Min { get; set; }
+        }
+
+        private readonly Stack<Node> _stack;
+
+        public MinStack()
+        {
+            _stack = new Stack<Node>();
+        }
+
+        public void Push(int x)
+        {
+            if (_stack.Count != 0)
+            {
+                var peek = _stack.Peek();
+
+                if (x >= peek.Min) return;
+
+                foreach (var node in _stack)
+                {
+                    node.Min = x;
+                }
+            }
+
+            var newNode = new Node
+            {
+                Item = x,
+                Min = x
+            };
+            _stack.Push(newNode);
+        }
+
+        public void Pop()
+        {
+            _stack.Pop();
+        }
+
+        public int Top()
+        {
+            return _stack.Count != 0 ? _stack.Peek().Item : 0;
+        }
+
+        public int GetMin()
+        {
+            return _stack.Count != 0 ? _stack.Peek().Min : 0;
+        }
+    }
+}
---
+++ stack/155_MinStack/MinStack/MinStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minStack = new MinStack();
+
+            minStack.Push(5);
+            minStack.Push(2);
+            minStack.Push(12);
+            minStack.Push(1);
+
+            Console.WriteLine(minStack.GetMin());
+            minStack.Pop();
+            minStack.Pop();
+            Console.WriteLine(minStack.GetMin());
+            Console.ReadLine();
+        }
+    }
+}
