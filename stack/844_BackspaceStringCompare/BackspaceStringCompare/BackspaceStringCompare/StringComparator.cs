dfced833fd3f80157d08cd1558a9a9a4d65e595f
viaches <i.am.viaches@gmail.com>
2018-08-05 20:45:37 +0300

    Added solution for 844. Backspace String Compare (https://leetcode.com/problems/backspace-string-compare/description/)

---
+++ stack/844_BackspaceStringCompare/BackspaceStringCompare/BackspaceStringCompare/StringComparator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace BackspaceStringCompare
+{
+    internal class StringComparator
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            return BuildResultString(S) == BuildResultString(T);
+        }
+        public string BuildResultString(string s)
+        {
+            StringBuilder res = new StringBuilder();
+            foreach (var c in s)
+            {
+                if (c == '#')
+                {
+                    if (res.Length > 0)
+                    {
+                        res.Remove(res.Length - 1, 1);
+                    }
+                }
+                else
+                {
+                    res.Append(c);
+                }
+            }
+            return res.ToString();
+        }
+    }
+}
