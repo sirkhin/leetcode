9bc1a115568735b85c2953a6a1970dbe58e27cb0
viaches <i.am.viaches@gmail.com>
2018-08-05 17:43:14 +0300

    Added solution for 682: Baseball game (https://leetcode.com/problems/baseball-game/description/)

---
+++ stack/682_BaseballGame/BaseballGame/BaseballGame/ArrayStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace BaseballGame
+{
+    public class ArrayStack
+    {
+        private int[] _array;
+
+        private int _current = 0;
+
+        private int _sum = 0;
+
+        public ArrayStack()
+        {
+            _array = new int[1];
+        }
+
+        public void Push(int item)
+        {
+            _array[_current++] = item;
+
+            _sum += item;
+
+            if (_current == _array.Length)
+            {
+                IncreaseArraySize();
+            }
+        }
+
+        private void IncreaseArraySize()
+        {
+            Array.Resize(ref _array, 2 * _array.Length);
+        }
+
+        public int Pop()
+        {
+            var item = _array[_current - 1];
+
+            _array[_current-- - 1] = 0;
+
+            _sum -= item;
+
+            if (_current == _array.Length / 4)
+            {
+                Array.Resize(ref _array, _array.Length / 2);
+            }
+
+            return item;
+        }
+        
+        public int Sum()
+        {
+            return _sum;
+        }
+
+    }
+}
