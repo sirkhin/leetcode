615cff2679c48c40320a27e7b1e335aacdd59a96
viaches <i.am.viaches@gmail.com>
2018-08-05 21:47:54 +0300

    Solution added for 232. Implement Queue using Stacks (https://leetcode.com/problems/implement-queue-using-stacks/description/)

---
+++ queue/232_QueueUsingStacks/QueueUsingStacks/QueueUsingStacks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stacksQueue = new StacksQueue();
+
+            stacksQueue.Push(1);
+            stacksQueue.Push(2);
+            stacksQueue.Push(3);
+            stacksQueue.Push(4);
+
+            Console.WriteLine(stacksQueue.Pop());
+            Console.WriteLine(stacksQueue.Peek());
+            Console.WriteLine(stacksQueue.Empty());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ queue/232_QueueUsingStacks/QueueUsingStacks/QueueUsingStacks/StacksQueue.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+
+namespace QueueUsingStacks
+{
+    internal class StacksQueue
+    {
+        private readonly Stack _pushStack;
+
+        private readonly Stack _popStack;
+
+        public StacksQueue()
+        {
+            _pushStack = new Stack();
+            _popStack = new Stack();
+        }
+
+        public void Push(int x)
+        {
+            if (_popStack.Count == 0)
+            {
+                _pushStack.Push(x);
+            }
+            else
+            {
+                while (_popStack.Count != 0)
+                {
+                    _pushStack.Push(_popStack.Pop());
+                }
+
+                _pushStack.Push(x);
+            }
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            if (_popStack.Count != 0)
+            {
+                var item = _popStack.Pop();
+                return (int) item;
+            }
+            else
+            {
+                PushAllToPopStack();
+
+                var item = _popStack.Pop();
+                return (int) item;
+            }
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            if (_popStack.Count != 0) return (int) _popStack.Peek();
+
+            PushAllToPopStack();
+
+            return (int) _popStack.Peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            return _pushStack.Count == 0 && _popStack.Count == 0;
+        }
+
+        private void PushAllToPopStack()
+        {
+            while (_pushStack.Count != 0)
+            {
+                _popStack.Push(_pushStack.Pop());
+            }
+        }
+    }
+}
